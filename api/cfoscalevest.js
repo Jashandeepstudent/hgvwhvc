ffa493e35e269b718c97c7448d687e57124e5eb2
Jashandeepstudent <mrjolox@gmail.com>
2026-01-07 18:10:18 +0530

    Update cfoscalevest.js

--- api/cfoscalevest.js
+++ api/cfoscalevest.js
@@ -6,7 +6,7 @@ export const config = {
 };
 
 const corsHeaders = {
-  'Access-Control-Allow-Origin': 'http://upscalevest.site',
+  'Access-Control-Allow-Origin': 'httpS://upscalevest.site',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
   'Access-Control-Max-Age': '86400',
